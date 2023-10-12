dd117848ad1e9e3a1d147e0fc8a278f91e5532e4
Joel Amoako <joelkofiamoako@gmail.com>
2023-10-12 22:06:23 +0000

    Quick Auto Commit

---
+++ src/main/java/com/boot/tax/calculator/gh/GhanaTaxCalculatorApiApplication.java
@@ -0,0 +1,13 @@
+package com.boot.tax.calculator.gh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GhanaTaxCalculatorApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GhanaTaxCalculatorApiApplication.class, args);
+	}
+
+}
